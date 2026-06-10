78b84a6d880f471f3afcd662948219e3553f9be9
Федяев Роман Валерьевич <fromroman@mail.ru>
2026-06-10 10:35:34 +0300

    add 1.02.01/02

---
+++ 1.2.01/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::setlocale(LC_ALL, "Russian");
+
+    double number;
+
+    std::cout << "Введите число:\n";
+    std::cin >> number;
+    std::cout << "Вы ввели:\n" << number;
+}
+
---
+++ 1.2.02/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    char oneWord[30];
+
+    std::cout << "Введите слово:\n";
+    std::cin >> oneWord;
+    std::cout << "Вы ввели:\n" << oneWord;
+}
