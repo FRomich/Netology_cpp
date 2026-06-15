c9611efdb8c7c66a01fc79e28b47c6a8834fbf96
Roma <fromroman@mail.ru>
2026-06-15 21:30:03 +0300

    fix boolalpha

--- 1.3.01/main.cpp
+++ 1.3.01/main.cpp
@@ -6,16 +6,17 @@ int main()
 
     bool varBool = true;
 
+    std::cout << std::boolalpha;
     std::cout << "Оператор: ||\n";
-    std::cout << std::boolalpha << varBool << '\t' << varBool << '\t' << (varBool || varBool) << '\n';
-    std::cout << std::boolalpha << !varBool << '\t' << varBool << '\t' << (!varBool || varBool) << '\n';
-    std::cout << std::boolalpha << varBool << '\t' << !varBool << '\t' << (varBool || !varBool) << '\n';
-    std::cout << std::boolalpha << !varBool << '\t' << !varBool << '\t' << (!varBool || !varBool) << '\n';
+    std::cout << varBool << '\t' << varBool << '\t' << (varBool || varBool) << '\n';
+    std::cout << !varBool << '\t' << varBool << '\t' << (!varBool || varBool) << '\n';
+    std::cout << varBool << '\t' << !varBool << '\t' << (varBool || !varBool) << '\n';
+    std::cout << !varBool << '\t' << !varBool << '\t' << (!varBool || !varBool) << '\n';
     std::cout << '\n';
     std::cout << "Оператор: &&\n";
-    std::cout << std::boolalpha << varBool << '\t' << varBool << '\t' << (varBool && varBool) << '\n';
-    std::cout << std::boolalpha << !varBool << '\t' << varBool << '\t' << (!varBool && varBool) << '\n';
-    std::cout << std::boolalpha << varBool << '\t' << !varBool << '\t' << (varBool && !varBool) << '\n';
-    std::cout << std::boolalpha << !varBool << '\t' << !varBool << '\t' << (!varBool && !varBool) << '\n';
+    std::cout << varBool << '\t' << varBool << '\t' << (varBool && varBool) << '\n';
+    std::cout << !varBool << '\t' << varBool << '\t' << (!varBool && varBool) << '\n';
+    std::cout << varBool << '\t' << !varBool << '\t' << (varBool && !varBool) << '\n';
+    std::cout << !varBool << '\t' << !varBool << '\t' << (!varBool && !varBool) << '\n';
 }
 
