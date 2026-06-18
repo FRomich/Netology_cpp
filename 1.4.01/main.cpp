fc212d48de7a254aabbf9731c1be332b1411152d
Федяев Роман Валерьевич <fromroman@mail.ru>
2026-06-18 16:48:57 +0300

    add 1.4.01/02

---
+++ 1.4.01/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::setlocale(LC_ALL, "Russian");
+
+    int number = 0;
+	int sum = 0;
+
+    do
+    {
+        std::cout << "Введите целое число или число '0', чтобы закончить:\n";
+        std::cin >> number;
+        sum += number;
+    }
+	while (number != 0);
+
+    std::cout << "Сумма: " << sum;    
+}
+
---
+++ 1.4.02/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::setlocale(LC_ALL, "Russian");
+
+    int number = 0;
+    int sumDigit = 0;
+    int countDigit = 0;
+
+    std::cout << "Введите целое число:\n";
+    std::cin >> number;
+
+    countDigit = std::to_string(abs(number)).length();
+
+    while (countDigit-- > 0 )
+    {
+        sumDigit += number / pow(10, countDigit);
+        number = number % 10;
+    }
+
+//    while (number / 10 != 0)
+//    {
+//        sumDigit += 1;
+//        number = number % 10;
+//    }
+//
+//    std::cout << "Сумма цифр: " << sumDigit;
+}
+
