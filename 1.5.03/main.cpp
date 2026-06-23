d0a160aa34be12bfc0408f669d14caed28c18926
Федяев Роман Валерьевич <fromroman@mail.ru>
2026-06-23 08:59:09 +0300

    fix 1.5.03

--- 1.5.03/main.cpp
+++ 1.5.03/main.cpp
@@ -11,30 +11,28 @@ int main()
     int minNumber = std::numeric_limits<int>::max();
     const int col = 6, row = 3;
     int arrayNumbers[col][row];
-   
 
     std::cout << "Массив:\n";
     for (int i = 0; i < row; i++)
     {
-	    for (int j = 0; j< col; j++)
-	    {
+        for (int j = 0; j < col; j++)
+        {
             arrayNumbers[i][j] = std::rand() % 100;
             std::cout << arrayNumbers[i][j] << "\t";
-            if (arrayNumbers[i][j] > maxNumber) 
+            if (arrayNumbers[i][j] > maxNumber)
             {
                 idxMaxNum = std::to_string(i) + " " + std::to_string(j);
                 maxNumber = arrayNumbers[i][j];
             }
-            if (arrayNumbers[i][j] < minNumber) 
+            if (arrayNumbers[i][j] < minNumber)
             {
                 idxMinNum = std::to_string(i) + " " + std::to_string(j);
                 minNumber = arrayNumbers[i][j];
             }
-	    }
+        }
         std::cout << "\n";
     }
 
     std::cout << "Индекс минимального элемента: " << idxMinNum << "\n";
-    std::cout << "Индекс максимального элемента: " << idxMaxNum<< "\n";
-
+    std::cout << "Индекс максимального элемента: " << idxMaxNum << "\n";
 }
