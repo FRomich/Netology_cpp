47427b7211cfaaf67e5eb5147afd7ada93e1759d
Roma <fromroman@mail.ru>
2026-06-25 07:36:04 +0300

    add 1.6.01/03

---
+++ 1.6.02/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+void myPow(int value, int power)
+{
+	int result = 1;
+
+	for (int i = 0; i < power; i++)
+	{
+		result *= value;
+	}
+
+	std::cout << value << " в степени " << power << " = " << result << "\n";
+}
+
+int main()
+{
+	std::setlocale(LC_ALL, "Russian");
+
+	int value = 5, power = 2;
+
+    myPow(value, power);
+    
+	value = 3, power = 3;
+
+	myPow(value, power);
+	
+	value = 4, power = 4;
+
+	myPow(value, power);
+}
---
+++ 1.6.03/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+std::string numbersFibonacci (int count)
+{
+    long long previous = 0;
+    long long forward = 1;
+    std::string result { std::to_string(previous) + " " + std::to_string(forward) + " " };
+
+    for (int i = 2; i < count; i++)
+    {
+        std::swap(previous, forward);
+        forward += previous;
+        result += std::to_string(forward) + " ";
+    }
+    return  result;
+}
+
+int main()
+{
+    std::setlocale(LC_ALL, "Russian");
+
+    int countFibonacci;
+
+    std::cout << "Введите число: ";
+    std::cin >> countFibonacci;
+    std::cout << "Числа Фибоначчи: " << numbersFibonacci(countFibonacci);
+}
+
