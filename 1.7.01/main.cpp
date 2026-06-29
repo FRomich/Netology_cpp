67ee8af32f1042fd6efb737ca6e6944f59e6375e
Федяев Роман Валерьевич <fromroman@mail.ru>
2026-06-29 10:10:48 +0300

    add 1.7.01/02

---
+++ 1.7.01/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::setlocale(LC_ALL, "Russian");
+
+    short varShort;
+    int varInt;
+    long varLong;
+    long long varLongLong;
+    float varFloat;
+    double varDouble;
+    long double varLongDouble;
+    bool varBool;
+
+    std::cout << "short: " << &varShort << " " << sizeof(varShort) << "\n";
+    std::cout << "int: " << &varInt << " " << sizeof(varInt) << "\n";
+    std::cout << "long: " << &varLong << " " << sizeof(varLong) << "\n";
+    std::cout << "long long: " << &varLongLong << " " << sizeof(varLongLong) << "\n";
+    std::cout << "float: " << &varFloat << " " << sizeof(varFloat) << "\n";
+    std::cout << "double: " << &varDouble << " " << sizeof(varDouble) << "\n";
+    std::cout << "long double: " << &varLongDouble << " " << sizeof(varLongDouble) << "\n";
+    std::cout << "bool: " << &varBool << " " << sizeof(varBool) << "\n";
+}
---
+++ 1.7.02/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+/// <summary>
+/// XOR. For integers only
+/// </summary>
+static void swap(int& a, int& b)
+{
+	a ^= b;
+	b ^= a;
+	a ^= b;
+}
+
+int main(int argc, char** argv)
+{
+	int a = 5, b = 8;
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	swap(a, b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	return 0;
+}
