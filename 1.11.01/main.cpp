87350cd9c5ddee5cb174dc0c1087b5d137491c82
Федяев Роман Валерьевич <fromroman@mail.ru>
2026-07-13 10:05:05 +0300

    add 1.11.01/02

---
+++ 1.11.01/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string firstName;
+    std::string lastName;
+    std::string targetString;
+
+    std::cout << "Введите имя: ";
+    std::cin >> firstName;
+    std::cout << "Введите фамилию: ";
+    std::cin >> lastName;
+
+    targetString = "Здравствуйте, " + firstName + " " + lastName + "!";
+
+    std::cout << targetString;
+}
---
+++ 1.11.02/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string mysteryWord {"малина"};
+    std::string userWord;
+
+    while (true)
+    {
+        std::cout << "Введите слово: ";
+        std::cin >> userWord;
+
+        if (userWord == mysteryWord)
+            break;
+
+        std::cout << "Неправильно\n";
+    }
+
+    std::cout << "Правильно! Вы победили! Загаданное слово — " << mysteryWord;
+}
